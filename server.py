dabf593dd9d8ad79c216785d740e7d1e6b3224a9
ImmortalProgrammer <cs01@tuta.io>
2024-02-22 20:54:40 -0500

    Docker Setup & Server.py Fix
    Signed-off-by: ImmortalProgrammer <cs01@tuta.io>

--- server.py
+++ server.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+
+@app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    return 'Login'
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return 'Register'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
