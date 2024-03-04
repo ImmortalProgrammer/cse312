a1cd811da02381cd1c13ac7aa3d20229121c7c98
ImmortalProgrammer <cs01@tuta.io>
2024-03-03 20:59:32 -0500

    database + register
    Signed-off-by: ImmortalProgrammer <cs01@tuta.io>

--- server.py
+++ server.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template, request
+from pymongo import MongoClient
+import bcrypt
+
+import misc
 
 app = Flask(__name__)
 
+mongo_client = MongoClient("mongo")
+db = mongo_client["cse312"]
+user_collection = db['users']
 
 @app.after_request
 def header(response):
     response.headers['X-Content-Type-Options'] = 'nosniff'
     return response
+
 
 @app.route('/')
 def index():
@@ -17,10 +25,30 @@ def index():
 def login():
     return 'Login'
 
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    return 'Register'
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        password_confirm = request.form['password_confirm']
+
+        if password != password_confirm:
+            return 'Two Entered Passwords not the same', 400
+
+        if user_collection.find_one({'username': username}):
+            return 'Username already exists', 400
+
+        if not misc.is_valid_password(password):
+            return "Password must be 8 characters long, one uppercase letter, one lowercase letter, one digit, " \
+                   "and one special character", 400
+
+        salt = bcrypt.gensalt()
+
+        hashed_pwd = bcrypt.hashpw(password.encode(), salt)
+
+        user_collection.insert_one({'username': username, 'password': hashed_pwd, 'salt': salt})
+
+        return render_template('login.html')
 
 
 if __name__ == "__main__":
